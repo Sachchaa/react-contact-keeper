3464fe4aa938da7646c48c4196d12ba9068e2e8e
Sachchaa <shewahal@deakin.edu.au>
2021-02-24 23:31:46 +1100

    Contact-keeper updated -
    Started working with Authentication
    Created Auth context

--- client/src/App.js
+++ client/src/App.js
@@ -5,26 +5,28 @@ import NavbarComponent from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
 import ContactState from './context/contact/ContactState'
+import AuthState from './context/auth/AuthState'
 
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <>
-          <NavbarComponent />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </>
-      </Router>
-    </ContactState>
-
+    <AuthState>
+      <ContactState>
+        <Router>
+          <>
+            <NavbarComponent />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+              </Switch>
+            </div>
+          </>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
